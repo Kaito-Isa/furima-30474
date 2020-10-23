02149196fd4202433691f929534a273ab6a5cdf1
Kaito-Isa <geraniu6@gmail.com>
2020-10-23 18:47:58 +0900

    ユーザー管理機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(item_params)
+    if @user.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:user).permit(:nick_name, :email, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date)
+  end
 end
--- db/migrate/20201020093618_devise_create_users.rb
+++ db/migrate/20201020093618_devise_create_users.rb
@@ -6,9 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nick_name,          null: false
       t.string :first_name,         null: false
-      t.string :last_naem,          null: false
+      t.string :last_name,          null: false
       t.string :first_name_kana,    null: false
-      t.string :last_naem_kane,     null: false
+      t.string :last_name_kana,     null: false
       t.date :birth_date,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
