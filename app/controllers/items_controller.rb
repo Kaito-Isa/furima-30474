5fd05e0074eb0b081bec447108671a4b787c942d
Kaito-Isa <geraniu6@gmail.com>
2020-11-06 19:20:53 +0900

    指摘箇所修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
 
@@ -20,18 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-     @item = Item.find(params[:id])
   end
 
   def edit
-    # @item =Item.find(params[:id])
     # unless current_user == @item.user
       # redirect_to root_path
     # end
   end
 
   def update
-    # item = Item.find(params[:id])
     # if item.update(item_params)
       # redirect_to item_path(@item)
     # else
@@ -40,7 +38,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    # item = Item.find(params[:id])
     # item.destroy
     # redirect_to root_path
   end
@@ -49,5 +46,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name,:description,:category_id,:condition_id,:burden_id,:area_id,:days_id,:price,:image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
